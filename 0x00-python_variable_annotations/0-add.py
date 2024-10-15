b2577ec9349eaeb12e6ebccb733624aabf13e385
Tega96 <otiteotega@gmail.com>
2024-10-15 20:19:39 +0000

    add two numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float)-> float:
+    """
+    Adds two numbers of type float and return sum as a float
+
+    Parameters:
+    a (float): first number.
+    b (float): second number.
+
+    Returns:
+    float: the sum of a and b
+    """
+    return a + b
