98b835ecd07fb11ad145ca742c242e7b72f4bcc3
Tega96 <otiteotega@gmail.com>
2024-11-05 03:41:29 +0000

    Unittest and integration tests

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" A function that test the utils.py file."""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test class for the access_nested_map function from the utils module.
+    Inherits form unittest.TestCase.
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test the access-nested_map function with different imputs.
+        Parameters:
+        nested_map (Mapping): The nested map to access.
+        path (Sequence): The sequece of keys representing the path to the value.
+        expected (Any): The expected result from the function.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+    if __name__ = "__main__":
+        unittest.main()
